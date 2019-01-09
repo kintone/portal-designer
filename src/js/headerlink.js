44a261dce825197c42d98e34e2c9d7edebe7d85f
koba <daisuke-kobayashi@cybozu.co.jp>
2019-01-09 18:26:06 +0900

    HeaderLinkをモジュール化

---
+++ src/js/headerlink.js
@@ -0,0 +1,16 @@
+export class HeaderLink {
+
+    constructor(){};
+
+    render() {
+        const anchorEl = document.createElement('a');
+        anchorEl.className = 'customize-portal';
+        anchorEl.setAttribute('href', chrome.extension.getURL('html/customize.html'));
+        anchorEl.setAttribute('target', '_blank');
+        anchorEl.setAttribute('aria-label', 'Customize Portal');
+        anchorEl.setAttribute('title', 'Customize portal');
+
+        const linksEl = document.querySelector('.gaia-header-toolbar-links');
+        linksEl && linksEl.appendChild(anchorEl);
+    }
+}
