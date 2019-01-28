2deed032cc688da022f9be60bec9532fedd0cc01
koba <daisuke-kobayashi@cybozu.co.jp>
2019-01-28 21:32:55 +0900

    Chage Header & Toolbar Colors as settings

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,26 @@ function addDefaultClassToBody(type) {
     document.body.classList.add('kintone-portal-default');
 }
 
+function _updateHeaderColor(headerColor) {
+    document.querySelector('.gaia-header-header').style = 'background-color: ' + headerColor;
+}
+
+function _updateToolbarColor(toolbarColor) {
+    document.querySelector('.gaia-header-toolbar').style = 'background-color: ' + toolbarColor;
+}
+
 function renderCustomizedPortal() {
     if (!isPortalPage()) {
         return;
     }
-    chrome.storage.local.get(['type', 'html', 'css', 'js'], function (value) {
+    chrome.storage.local.get(['type', 'html', 'css', 'js', 'headerColor', 'toolbarColor'], (value) => {
         if (!value.type || value.type === 'default') {
             addDefaultClassToBody();
             return;
         }
         new CustomizedPortal(value.html, value.css, value.js).render();
+        _updateHeaderColor(value.headerColor);
+        _updateToolbarColor(value.toolbarColor);
     });
 }
 
