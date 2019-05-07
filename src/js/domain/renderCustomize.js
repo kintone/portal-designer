a6dd7b0e64b24b38948c5875edbfb940ff5598fb
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-07 22:01:42 +0900

    スタイルが二重についてしまうのを解消

--- src/js/domain/renderCustomize.js
+++ src/js/domain/renderCustomize.js
@@ -3,6 +3,8 @@ import KintonePortalElements from '../lib/KintonePortalElements';
 import KintoneUrl from '../lib/KintoneUrl';
 import waitPortalShow from '../lib/waitPortalShow';
 
+const KINTONE_PORTAL_CUSTOMIZE_STYLE = 'kintone-portal-customize-style';
+
 const hideWidgets = () => {
   const bodyEl = KintonePortalElements.getPortalBodyElement();
   bodyEl.style.display = 'none';
@@ -13,10 +15,20 @@ const hidePortalIndexHeader = () => {
   headerEl.style.display = 'none';
 };
 
+const removeStyleElement = () => {
+  const styleEl = document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_STYLE)[0];
+  if (styleEl) {
+    styleEl.remove();
+  }
+};
+
 const renderCustomizeTo = ({ html, css, js }, entryPointEl) => {
+  removeStyleElement();
+
   const headEl = document.querySelector('head');
   const styleEl = document.createElement('style');
   styleEl.innerHTML = css;
+  styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_STYLE);
   headEl.appendChild(styleEl);
 
   const innerEl = document.createElement('div');
