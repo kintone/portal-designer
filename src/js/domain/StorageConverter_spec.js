add8337246d3f0c6e573a71acfd182560290a964
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-08 13:26:17 +0900

    testコマンドを作成

--- src/js/domain/StorageConverter_spec.js
+++ src/js/domain/StorageConverter_spec.js
@@ -1,12 +1,19 @@
 /* eslint-env mocha */
 
+import { expect } from 'chai';
 import { TYPE_CUSTOMIZE, TYPE_DEFAULT } from './Storage';
 import { convertStorageToType } from './StorageConverter';
 
 describe('StorageConverter', () => {
   context('convertStorageToType', () => {
     it('TYPE_CUSTOMIZEには、TYPE_CUSTOMIZEを返す', () => {
-      convertStorageToType(TYPE_CUSTOMIZE);
+      const actual = convertStorageToType({ type: TYPE_CUSTOMIZE });
+      expect(actual).eql(TYPE_CUSTOMIZE);
+    });
+
+    it('TYPE_CUSTOMIZEには、TYPE_DEFAULTを返す', () => {
+      const actual = convertStorageToType({ type: TYPE_DEFAULT });
+      expect(actual).eql(TYPE_DEFAULT);
     });
   });
 });
