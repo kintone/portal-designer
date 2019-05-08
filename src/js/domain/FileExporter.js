97f7d5d08f3a49f08b0930292bf44f48449dd4bb
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-08 15:37:08 +0900

    FileExporterのテストを作成

--- src/js/domain/FileExporter.js
+++ src/js/domain/FileExporter.js
@@ -1,11 +1,20 @@
-const DEFAULT_FILE_NAME = 'customize.json';
+import sanitize from 'sanitize-filename';
 
-// eslint-disable-next-line
+export const FILE_EXT = 'json';
+export const DEFAULT_FILE_NAME = `customize.${FILE_EXT}`;
+
+export const generateFileName = (optFileName) => {
+  let fileName = optFileName || DEFAULT_FILE_NAME;
+  if (!/.*\.json$/.test(fileName)) {
+    fileName = `${fileName}.${FILE_EXT}`;
+  }
+  return sanitize(fileName, { replacement: '_' });
+};
+
 export const exportFile = (text, optFileName) => {
-  const fileName = optFileName || DEFAULT_FILE_NAME;
   const element = document.createElement('a');
   element.setAttribute('href', `data:text/plain;charset=utf-8,${text}`);
-  element.setAttribute('download', fileName);
+  element.setAttribute('download', generateFileName(optFileName));
   element.setAttribute('hidden', 'hidden');
 
   document.body.appendChild(element);
