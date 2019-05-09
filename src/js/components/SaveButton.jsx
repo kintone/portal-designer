8eaca244b511c15bdc3992bc484164f83f021af5
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-09 13:20:27 +0900

    StoreとPageComponentのStateをマージして保存するようにした

--- src/js/components/SaveButton.jsx
+++ src/js/components/SaveButton.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { EditorStore } from '../EditorStore';
 import Storage from '../domain/Storage';
 import { convertStateToStorage } from '../domain/StorageConverter';
 
 const SaveButton = (props) => {
-  const onClick = async (state) => {
-    const storageValue = convertStateToStorage(state);
+  const { state } = useContext(EditorStore);
+
+  // TODO: React Hooksへの移行が完了したら破棄する
+  const mergeState = (storeState, pageState) => {
+    const mergedState = { ...pageState };
+    mergedState.editor.headerColor = storeState.editor.headerColor;
+    mergedState.editor.toolbarColor = storeState.editor.toolbarColor;
+    return mergedState;
+  };
+
+  const onClick = async (pageState) => {
+    const mergedState = mergeState(state, pageState);
+    const storageValue = convertStateToStorage(mergedState);
     await Storage.set(storageValue);
   };
 
