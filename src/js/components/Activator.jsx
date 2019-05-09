fc9164927e2b911b60ef63c84704ba4b4369800c
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-09 20:00:34 +0900

    Checkboxコンポーネントを作成

--- src/js/components/Activator.jsx
+++ src/js/components/Activator.jsx
@@ -1,36 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Checkbox from './Checkbox';
 
-const Activator = (props) => {
-  // TODO: インスタンスごとに異なる値にする
-  const generateCheckBoxId = () => 'action-enable-customized-portal-input';
-
-  const handleChange = (evt) => {
-    props.onChange(evt.target.checked);
-  };
-
-  return (
-    <div className="action-enable-customized-portal">
-      <input
-        type="checkbox"
-        className="action-enable-customized-portal-input"
-        id={generateCheckBoxId()}
-        checked={props.enabled}
-        onChange={evt => handleChange(evt)}
-      />
-      <label // eslint-disable-line
-        htmlFor={generateCheckBoxId()}
-        className="action-enable-customized-portal-label"
-      >
-      Enable customized portal
-      </label>
-    </div>
-  );
-};
+const Activator = props => (
+  <Checkbox
+    {...props}
+    checked={props.enabled}
+    label="Enable customized portal"
+    className="action-enable-customized-portal"
+  />
+);
 
 Activator.propTypes = {
   enabled: PropTypes.bool,
-  onChange: PropTypes.func.isRequired,
 };
 
 Activator.defaultProps = {
---
+++ src/js/components/Checkbox.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Checkbox = (props) => {
+  // TODO: インスタンスごとに異なる値にする
+  const generateCheckBoxId = () => props.className;
+
+  const handleChange = (evt) => {
+    props.onChange(evt.target.checked);
+  };
+
+  return (
+    <div className={props.className}>
+      <input
+        type="checkbox"
+        id={generateCheckBoxId()}
+        className={`${props.className}-input`}
+        checked={props.checked}
+        onChange={evt => handleChange(evt)}
+      />
+      <label // eslint-disable-line
+        htmlFor={generateCheckBoxId()}
+        className={`${props.className}-label`}
+      >
+        {props.label}
+      </label>
+    </div>
+  );
+};
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Checkbox.defaultProps = {
+  checked: false,
+  className: 'checkbox',
+};
+
+export default Checkbox;
