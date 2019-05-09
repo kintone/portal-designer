bc4f995e6b4cf67c6a5cbfc33c6543d34213dfb2
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-09 19:44:07 +0900

    ActivatorをFunctional Componentに変更

--- src/js/components/Activator.jsx
+++ src/js/components/Activator.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Activator extends Component {
-  static generateCheckBoxId() {
-    // TODO: インスタンスごとに異なる値にする
-    return 'action-enable-customized-portal-input';
-  }
+const Activator = (props) => {
+  // TODO: インスタンスごとに異なる値にする
+  const generateCheckBoxId = () => 'action-enable-customized-portal-input';
 
-  handleChange(evt) {
-    this.props.onChange(evt.target.checked);
-  }
+  const handleChange = (evt) => {
+    props.onChange(evt.target.checked);
+  };
 
-  render() {
-    return (
-      <div className="action-enable-customized-portal">
-        <input
-          type="checkbox"
-          className="action-enable-customized-portal-input"
-          id={Activator.generateCheckBoxId()}
-          checked={this.props.enabled}
-          onChange={evt => this.handleChange(evt)}
-        />
-        <label // eslint-disable-line
-          htmlFor={Activator.generateCheckBoxId()}
-          className="action-enable-customized-portal-label"
-        >
-        Enable customized portal
-        </label>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="action-enable-customized-portal">
+      <input
+        type="checkbox"
+        className="action-enable-customized-portal-input"
+        id={generateCheckBoxId()}
+        checked={props.enabled}
+        onChange={evt => handleChange(evt)}
+      />
+      <label // eslint-disable-line
+        htmlFor={generateCheckBoxId()}
+        className="action-enable-customized-portal-label"
+      >
+      Enable customized portal
+      </label>
+    </div>
+  );
+};
 
 Activator.propTypes = {
   enabled: PropTypes.bool,
