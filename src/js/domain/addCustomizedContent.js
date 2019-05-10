a72e683b5c85a3fb4523cf0916d7a151af97c853
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-10 20:09:21 +0900

    要素をそれぞれ消す

--- src/js/domain/addCustomizedContent.js
+++ src/js/domain/addCustomizedContent.js
@@ -1,29 +1,35 @@
 import KintonePortalElements from '../lib/KintonePortalElements';
 
-const KINTONE_PORTAL_CUSTOMIZE = 'kintone-portal-customize';
+const KINTONE_PORTAL_CUSTOMIZE_HTML = 'kintone-portal-customize-html';
+const KINTONE_PORTAL_CUSTOMIZE_CSS = 'kintone-portal-customize-css';
+const KINTONE_PORTAL_CUSTOMIZE_JS = 'kintone-portal-customize-js';
 
 const addCustomizedContentTo = (model, entryPointEl) => {
   const headEl = document.querySelector('head');
   const styleEl = document.createElement('style');
   styleEl.innerHTML = model.css;
-  styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE);
+  styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_CSS);
   headEl.appendChild(styleEl);
 
   const innerEl = document.createElement('div');
   innerEl.innerHTML = model.html;
-  styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE);
+  innerEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_HTML);
   entryPointEl.appendChild(innerEl);
 
   const scriptEl = document.createElement('script');
   scriptEl.innerHTML = model.js;
-  scriptEl.classList.add(KINTONE_PORTAL_CUSTOMIZE);
+  scriptEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_JS);
 
   entryPointEl.appendChild(scriptEl);
 };
 
 const removeCustomizedContent = () => {
-  const customizedEls = document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE);
-  Array.prototype.forEach.call(customizedEls, el => el.remove());
+  const customizedEls = [
+    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_JS)[0],
+    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_HTML)[0],
+    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_CSS)[0],
+  ];
+  customizedEls.filter(el => !!el).forEach(el => el.remove());
 };
 
 export default (model) => {
