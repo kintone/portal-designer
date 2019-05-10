bb981c55ceda66677df8ed66f9ce2f06d08f0ab8
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-10 14:04:59 +0900

    NameInput_specをTextInput_specに移行

--- src/js/components/TextInput_spec.jsx
+++ src/js/components/TextInput_spec.jsx
@@ -4,14 +4,13 @@ import { expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import NameInput from './NameInput';
+import TextInput from './TextInput';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('NameInput', () => {
   it('render', () => {
-    const props = {};
-    const nameInput = shallow(<NameInput {...props} />);
+    const nameInput = shallow(<TextInput />);
     expect(nameInput).to.be.an('object');
   });
 });
