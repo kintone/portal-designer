164b596ee479e2d9521df137c4fcacc0fd178fa1
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-11 22:30:13 +0900

    @types/react @types/react-domをインストール

---
+++ src/js/components/TextInput.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+const TextInput = props => {
+  const handleChange = evt => {
+    props.onChange(evt.target.value);
+  };
+
+  return (
+    <input
+      className={props.className}
+      type="text"
+      placeholder={props.placeholder}
+      aria-label={props.label}
+      value={props.value}
+      onChange={handleChange}
+    />
+  );
+};
+
+TextInput.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+TextInput.defaultProps = {
+  className: "text-input",
+  value: "",
+  placeholder: ""
+};
+
+export default TextInput;
