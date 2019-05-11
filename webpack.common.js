61c426023710d832112a315574572ef958de703a
koba <daisuke-kobayashi@cybozu.co.jp>
2019-05-11 16:14:03 +0900

    eslint fix(webpack.js)

--- webpack.common.js
+++ webpack.common.js
@@ -1,70 +1,74 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+/* eslint-env node */
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    'js/index': './src/js/index.js',
-    'js/editor': './src/js/Editor.jsx',
-    'js/popup': './src/js/popup.js',
-    'css/index': './src/scss/index.scss',
-    'css/editorpage': './src/scss/editorpage.scss',
-    'css/popup': './src/scss/popup.scss',
+    "js/index": "./src/js/index.js",
+    "js/editor": "./src/js/Editor.jsx",
+    "js/popup": "./src/js/popup.js",
+    "css/index": "./src/scss/index.scss",
+    "css/editorpage": "./src/scss/editorpage.scss",
+    "css/popup": "./src/scss/popup.scss"
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist")
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
-    new CopyWebpackPlugin([{
-      from: 'src/img',
-      to: 'img',
-    }]),
-    new CopyWebpackPlugin([{
-      from: 'src/html',
-      to: 'html',
-    }]),
-    new CopyWebpackPlugin([{
-      from: 'src/manifest.json',
-      to: 'manifest.json',
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: "src/img",
+        to: "img"
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: "src/html",
+        to: "html"
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: "src/manifest.json",
+        to: "manifest.json"
+      }
+    ])
   ],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '../img/[name].[ext]',
-              outputPath: '../dist/img',
-            },
-          },
-        ],
+              name: "../img/[name].[ext]",
+              outputPath: "../dist/img"
+            }
+          }
+        ]
       },
       {
         test: /\.jsx$/,
         use: [
           {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-    ],
-  },
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  }
 };
