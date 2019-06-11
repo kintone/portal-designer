7261705e1b58b05414430fa64e7450b5f58d0b37
sukoyakarizumu <sukoyakarizumu@gmail.com>
2019-06-11 22:10:36 +0900

    FileExporterの引数名とテストを修正

--- src/js/domain/FileExporter.ts
+++ src/js/domain/FileExporter.ts
@@ -1,17 +1,14 @@
 import sanitize from "sanitize-filename";
 
-const generateFileName = (fileName: string, ext: string) => {
-  const hasExt = new RegExp(".*." + ext + "$");
-  if (!hasExt.test(fileName)) {
-    fileName = `${fileName}.${ext}`;
-  }
+const generateFileName = (designName: string, suffix: string, ext: string) => {
+  const fileName = `${designName}${suffix}.${ext}`;
   return sanitize(fileName, { replacement: "_" });
 };
 
-const exportFile = (text: string, fileName: string, ext: string) => {
+const exportFile = (fileBody: string, fileName: string) => {
   const element = document.createElement("a");
-  element.setAttribute("href", `data:text/plain;charset=utf-8,${text}`);
-  element.setAttribute("download", generateFileName(fileName, ext));
+  element.setAttribute("href", `data:text/plain;charset=utf-8,${fileBody}`);
+  element.setAttribute("download", fileName);
   element.setAttribute("hidden", "hidden");
 
   document.body.appendChild(element);
@@ -19,26 +16,30 @@ const exportFile = (text: string, fileName: string, ext: string) => {
   document.body.removeChild(element);
 };
 
-const exportAsJson = (text: string, fileName: string) => {
-  exportFile(text, fileName || "customize", "json");
+const exportAsJson = (jsonString: string, designName: string) => {
+  const fileName = generateFileName(designName || "design", "", "json");
+  exportFile(jsonString, fileName);
 };
 
-const exportAsDesktopJS = async (text: string, fileName: string) => {
+const exportAsDesktopJS = async (jsonString: string, designName: string) => {
   const resp = await fetch(chrome.runtime.getURL("js/templates/desktop.js"));
   const template = await resp.text();
-  const jsString = template.replace("${renderingModel}", text);
-  exportFile(jsString, fileName || "desktop", "js");
+  const fileBody = template.replace("${renderingModel}", jsonString);
+  const fileName = generateFileName(designName || "design", "_desktop", "js");
+  exportFile(fileBody, fileName);
 };
 
-const exportAsMobileJS = async (text: string, fileName: string) => {
+const exportAsMobileJS = async (jsonString: string, designName: string) => {
   const resp = await fetch(chrome.runtime.getURL("js/templates/mobile.js"));
   const template = await resp.text();
-  const jsString = template.replace("${renderingModel}", text);
-  exportFile(jsString, fileName || "mobile", "js");
+  const fileBody = template.replace("${renderingModel}", jsonString);
+  const fileName = generateFileName(designName || "design", "_mobile", "js");
+  exportFile(fileBody, fileName);
 };
 
 export default {
   exportAsJson,
   exportAsDesktopJS,
-  exportAsMobileJS
+  exportAsMobileJS,
+  generateFileName
 };
--- src/js/domain/FileExporter_spec.ts
+++ src/js/domain/FileExporter_spec.ts
@@ -1,32 +1,30 @@
 /* eslint-env mocha */
 
 import * as chai from "chai";
-import { generateFileName } from "./FileExporter";
+import FileExporter from "./FileExporter";
 
 describe("FileExporter", () => {
   context("generateFileName", () => {
     it("通常のファイル名は、そのまま返る", () => {
       chai
-        .expect(generateFileName("original.json", "json"))
+        .expect(FileExporter.generateFileName("original", "", "json"))
         .eql("original.json");
-    });
-
-    it(".jsonの拡張子がない場合、.jsonが末尾につく", () => {
-      chai.expect(generateFileName("original", "json")).eql("original.json");
-    });
-
-    it("空文字の場合、デフォルトのファイル名になる", () => {
-      chai.expect(generateFileName("", "json")).eql(".json");
     });
 
     it("特殊文字はエスケープされる", () => {
       chai
-        .expect(generateFileName('/?<>\\:*|".json', "json"))
-        .eql("_________.json");
+        .expect(FileExporter.generateFileName("/?<>\\:*|", "", "json"))
+        .eql("________.json");
     });
 
     it("空白文字はそのまま", () => {
-      chai.expect(generateFileName(" .json", "json")).eql(" .json");
+      chai.expect(FileExporter.generateFileName(" ", "", "json")).eql(" .json");
+    });
+
+    it("接尾辞を指定できる", () => {
+      chai
+        .expect(FileExporter.generateFileName("original", "_desktop", "js"))
+        .eql("original_desktop.js");
     });
   });
 });
