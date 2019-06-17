180dfbbe998ece1487a91252754d2e230a1fbed5
sukoyakarizumu <sukoyakarizumu@gmail.com>
2019-06-17 14:46:40 +0900

    Popupの文言を修正

--- src/js/popup.ts
+++ src/js/popup.ts
@@ -3,9 +3,9 @@ import { convertStorageToType } from "./domain/StorageConverter";
 
 const initialize = async () => {
   const type = convertStorageToType((await Storage.getAll()).type);
-  const checkedRadioEl = <HTMLInputElement>(
-    document.querySelector(`.popup-radio-input[value=${type}]`)
-  );
+  const checkedRadioEl = document.querySelector(
+    `.popup-radio-input[value=${type}]`
+  ) as HTMLInputElement;
   checkedRadioEl.checked = true;
   const formEl = document.querySelector(".popup-form")!;
   formEl.addEventListener("change", (evt: any) => {
