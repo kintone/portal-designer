6bbe23c1012e0f7fdfaaf187b391999810539234
sukoyakarizumu <sukoyakarizumu@gmail.com>
2019-06-18 16:25:23 +0900

    定数名を変更

--- src/js/templates/mobile.js
+++ src/js/templates/mobile.js
@@ -3,34 +3,34 @@
 
   const renderingModel = ${renderingModel};
 
-  const KINTONE_PORTAL_CUSTOMIZE_HTML = "kintone-portal-customize-html";
-  const KINTONE_PORTAL_CUSTOMIZE_CSS = "kintone-portal-customize-css";
-  const KINTONE_PORTAL_CUSTOMIZE_JS = "kintone-portal-customize-js";
+  const KINTONE_PORTAL_DESIGNER_HTML = "kintone-portal-designer-html";
+  const KINTONE_PORTAL_DESIGNER_CSS = "kintone-portal-designer-css";
+  const KINTONE_PORTAL_DESIGNER_JS = "kintone-portal-designer-js";
 
   function addCustomizedContentTo(model, entryPointEl) {
     const headEl = document.querySelector("head");
     const styleEl = document.createElement("style");
     styleEl.innerHTML = model.css;
-    styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_CSS);
+    styleEl.classList.add(KINTONE_PORTAL_DESIGNER_CSS);
     headEl.appendChild(styleEl);
 
     const innerEl = document.createElement("div");
     innerEl.innerHTML = model.html;
-    innerEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_HTML);
+    innerEl.classList.add(KINTONE_PORTAL_DESIGNER_HTML);
     entryPointEl.appendChild(innerEl);
 
     const scriptEl = document.createElement("script");
     scriptEl.innerHTML = model.js;
-    scriptEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_JS);
+    scriptEl.classList.add(KINTONE_PORTAL_DESIGNER_JS);
 
     entryPointEl.appendChild(scriptEl);
   }
 
   function removeCustomizedContent() {
     const customizedEls = [
-      document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_JS)[0],
-      document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_HTML)[0],
-      document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_CSS)[0]
+      document.getElementsByClassName(KINTONE_PORTAL_DESIGNER_JS)[0],
+      document.getElementsByClassName(KINTONE_PORTAL_DESIGNER_HTML)[0],
+      document.getElementsByClassName(KINTONE_PORTAL_DESIGNER_CSS)[0]
     ];
     customizedEls
       .filter(function(el) {
