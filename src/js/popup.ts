d6f3b208487d1a43110b8f4be309d4d57fcb81cf
sukoyakarizumu <sukoyakarizumu@gmail.com>
2019-06-19 22:39:20 +0900

    ポップアップの中身をToggleスイッチにした

--- src/js/popup.ts
+++ src/js/popup.ts
@@ -1,15 +1,31 @@
-import Storage from "./domain/Storage";
+import Storage, { TYPE_CUSTOMIZE, TYPE_DEFAULT } from "./domain/Storage";
 import { convertStorageToType } from "./domain/StorageConverter";
 
+const getActivatorEl = () => {
+  return document.querySelector(".activator") as HTMLInputElement;
+};
+
+const renderActivator = async (enabled: boolean) => {
+  const activatorEl = getActivatorEl();
+  activatorEl.setAttribute("aria-pressed", enabled.toString());
+  activatorEl.textContent = enabled ? "Design Portal" : "Default Portal";
+};
+
+const loadEnabled = async () => {
+  return convertStorageToType((await Storage.getAll()).type) === TYPE_CUSTOMIZE;
+};
+
 const initialize = async () => {
-  const type = convertStorageToType((await Storage.getAll()).type);
-  const checkedRadioEl = document.querySelector(
-    `.popup-radio-input[value=${type}]`
-  ) as HTMLInputElement;
-  checkedRadioEl.checked = true;
-  const formEl = document.querySelector(".popup-form")!;
-  formEl.addEventListener("change", (evt: any) => {
-    Storage.set({ type: evt.target.value });
+  let enabled = await loadEnabled();
+  renderActivator(enabled);
+
+  getActivatorEl().addEventListener("click", () => {
+    enabled = !enabled;
+    renderActivator(enabled);
+    Storage.set({
+      type: enabled ? TYPE_CUSTOMIZE : TYPE_DEFAULT
+    });
   });
 };
+
 initialize();
