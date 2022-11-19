17bf6dcac969d55ad6cb7e85b7532ffc559c3e08
sukoyakarizumu <sukoyakarizumu@gmail.com>
2022-11-19 15:32:36 +0900

    manifest.jsonをV3に更新／ポータルのトップページのアイコンをCSSで表示する

--- src/js/domain/renderToolbarLink.ts
+++ src/js/domain/renderToolbarLink.ts
@@ -1,7 +1,7 @@
 const renderToolbarLink = () => {
   const anchorEl = document.createElement("a");
   anchorEl.className = "customize-portal";
-  anchorEl.setAttribute("href", chrome.extension.getURL("editor.html"));
+  anchorEl.setAttribute("href", chrome.runtime.getURL("editor.html"));
   anchorEl.setAttribute("target", "_blank");
   anchorEl.setAttribute("aria-label", "Kintone Portal Designer");
   anchorEl.setAttribute("title", "Kintone Portal Designer");
