f1cc0c36623063b70aff2be3a1b400cc82738292
sukoyakarizumu <sukoyakarizumu@gmail.com>
2022-11-19 15:31:26 +0900

    SCSSに関するモジュールを削除

--- webpack.common.js
+++ webpack.common.js
@@ -2,29 +2,19 @@
 
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
   entry: {
     "js/index": "./src/js/index.ts",
     "js/editor": "./src/js/Editor.tsx",
-    "js/popup": "./src/js/popup.ts",
-    "css/index": "./src/scss/index.scss",
-    "css/editor": "./src/scss/editor.scss",
-    "css/popup": "./src/scss/popup.scss"
+    "js/popup": "./src/js/popup.ts"
   },
   output: {
     path: path.join(__dirname, "dist")
   },
   plugins: [
     new MonacoWebpackPlugin(),
-    new FixStyleOnlyEntriesPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
     new CopyWebpackPlugin([
       {
         from: "src/img",
@@ -33,6 +23,10 @@ module.exports = {
       {
         from: "src/html",
         to: "html"
+      },
+      {
+        from: "src/css",
+        to: "css"
       },
       {
         from: "src/_locales/en",
@@ -61,14 +55,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      },
       {
         test: /\.(ts|tsx)$/,
         use: [
