8b328b74743fd1624bf7dff2e1f4976de7ebf39c
sukoyakarizumu <sukoyakarizumu@gmail.com>
2022-11-20 15:02:21 +0900

    StyledComponents移行: EditorPage

--- src/js/components/tabpanelcontents/ConfigTabPanelContent.tsx
+++ src/js/components/tabpanelcontents/ConfigTabPanelContent.tsx
@@ -1,35 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 import HeaderColorInput from "../../containers/tabpanelcontents/HeaderColorInput";
 import ToolbarColorInput from "../../containers/tabpanelcontents/ToolbarColorInput";
 import PortalHeaderHider from "../../containers/tabpanelcontents/PortalHeaderHider";
 import PortalHeaderColorInput from "../../containers/tabpanelcontents/PortalHeaderColorInput";
 
-const ConfigTabPanelContent = () => (
+const Component = ({ className }: { className: string }) => (
   <div>
     <details className="config-tabpanel-group" open>
       <summary className="config-tabpanel-group-label">
         {chrome.i18n.getMessage("kpd_unofficial_label")}
       </summary>
       <div className="config-tabpanel-group-content">
-        <p className="config-tabpanel-caution">
+        <p className={`${className}__config-tabpanel-caution`}>
           <strong>{chrome.i18n.getMessage("kpd_unofficial_caution")}: </strong>
           {chrome.i18n.getMessage("kpd_unofficial_caution_description")}
         </p>
-        <div className="config-tabpanel-row">
+        <div className={`${className}__config-tabpanel-row`}>
           <HeaderColorInput />
         </div>
-        <div className="config-tabpanel-row">
+        <div className={`${className}__config-tabpanel-row`}>
           <ToolbarColorInput />
         </div>
-        <div className="config-tabpanel-row">
+        <div className={`${className}__config-tabpanel-row`}>
           <PortalHeaderHider />
         </div>
-        <div className="config-tabpanel-row">
+        <div className={`${className}__config-tabpanel-row`}>
           <PortalHeaderColorInput />
         </div>
       </div>
     </details>
   </div>
 );
+
+const ConfigTabPanelContent = styled(Component)`
+  &__config-tabpanel-caution {
+    font-size: 1.1rem;
+    padding: 8px 0;
+  }
+
+  &__config-tabpanel-row {
+    font-size: 1rem;
+  }
+  &__config-tabpanel-row:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+`;
 
 export default ConfigTabPanelContent;
