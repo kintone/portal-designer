59b1473e4f73d7810e2f88884bbefe06cf266dbf
sukoyakarizumu <sukoyakarizumu@gmail.com>
2023-02-22 21:08:20 +0900

    Revert "StyledComponents移行: EditorPage"
    This reverts commit 8b328b74743fd1624bf7dff2e1f4976de7ebf39c.

--- src/js/components/tabpanelcontents/ConfigTabPanelContent.tsx
+++ src/js/components/tabpanelcontents/ConfigTabPanelContent.tsx
@@ -1,50 +1,35 @@
 import React from "react";
-import styled from "styled-components";
 import HeaderColorInput from "../../containers/tabpanelcontents/HeaderColorInput";
 import ToolbarColorInput from "../../containers/tabpanelcontents/ToolbarColorInput";
 import PortalHeaderHider from "../../containers/tabpanelcontents/PortalHeaderHider";
 import PortalHeaderColorInput from "../../containers/tabpanelcontents/PortalHeaderColorInput";
 
-const Component = ({ className }: { className: string }) => (
+const ConfigTabPanelContent = () => (
   <div>
     <details className="config-tabpanel-group" open>
       <summary className="config-tabpanel-group-label">
         {chrome.i18n.getMessage("kpd_unofficial_label")}
       </summary>
       <div className="config-tabpanel-group-content">
-        <p className={`${className}__config-tabpanel-caution`}>
+        <p className="config-tabpanel-caution">
           <strong>{chrome.i18n.getMessage("kpd_unofficial_caution")}: </strong>
           {chrome.i18n.getMessage("kpd_unofficial_caution_description")}
         </p>
-        <div className={`${className}__config-tabpanel-row`}>
+        <div className="config-tabpanel-row">
           <HeaderColorInput />
         </div>
-        <div className={`${className}__config-tabpanel-row`}>
+        <div className="config-tabpanel-row">
           <ToolbarColorInput />
         </div>
-        <div className={`${className}__config-tabpanel-row`}>
+        <div className="config-tabpanel-row">
           <PortalHeaderHider />
         </div>
-        <div className={`${className}__config-tabpanel-row`}>
+        <div className="config-tabpanel-row">
           <PortalHeaderColorInput />
         </div>
       </div>
     </details>
   </div>
 );
-
-const ConfigTabPanelContent = styled(Component)`
-  &__config-tabpanel-caution {
-    font-size: 1.1rem;
-    padding: 8px 0;
-  }
-
-  &__config-tabpanel-row {
-    font-size: 1rem;
-  }
-  &__config-tabpanel-row:not(:last-child) {
-    margin-bottom: 2rem;
-  }
-`;
 
 export default ConfigTabPanelContent;
