810298a63cd7c16d41aa072df87af918552537b9
sukoyakarizumu <sukoyakarizumu@gmail.com>
2024-10-25 20:57:04 +0900

    ts-ignoreを使う形でuserScriptsを使ってみた（無理だった）

--- src/js/domain/addCustomizedContent.ts
+++ src/js/domain/addCustomizedContent.ts
@@ -4,38 +4,41 @@ const KINTONE_PORTAL_CUSTOMIZE_HTML = "kintone-portal-customize-html";
 const KINTONE_PORTAL_CUSTOMIZE_CSS = "kintone-portal-customize-css";
 const KINTONE_PORTAL_CUSTOMIZE_JS = "kintone-portal-customize-js";
 
-const addCustomizedContentTo = (
-  model: RenderingModel,
-  entryPointEl: Element
-) => {
-  const headEl = document.querySelector("head")!;
-  const styleEl = document.createElement("style");
-  styleEl.innerHTML = model.css;
-  styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_CSS);
-  entryPointEl.appendChild(styleEl);
+// const addCustomizedContentTo = (
+//   model: RenderingModel,
+//   entryPointEl: Element
+// ) => {
+//   const headEl = document.querySelector("head")!;
+//   const styleEl = document.createElement("style");
+//   styleEl.innerHTML = model.css;
+//   styleEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_CSS);
+//   entryPointEl.appendChild(styleEl);
 
-  const innerEl = document.createElement("div");
-  innerEl.innerHTML = model.html;
-  innerEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_HTML);
-  entryPointEl.appendChild(innerEl);
+//   const innerEl = document.createElement("div");
+//   innerEl.innerHTML = model.html;
+//   innerEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_HTML);
+//   entryPointEl.appendChild(innerEl);
 
-  const scriptEl = document.createElement("script");
-  scriptEl.innerHTML = model.js;
-  scriptEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_JS);
+//   const scriptEl = document.createElement("script");
+//   scriptEl.innerHTML = model.js;
+//   scriptEl.classList.add(KINTONE_PORTAL_CUSTOMIZE_JS);
 
-  entryPointEl.appendChild(scriptEl);
-};
+//   entryPointEl.appendChild(scriptEl);
+// };
 
-const removeCustomizedContent = () => {
-  const customizedEls = [
-    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_JS)[0],
-    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_HTML)[0],
-    document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_CSS)[0]
-  ];
-  customizedEls.filter(el => !!el).forEach(el => el.remove());
-};
+// const removeCustomizedContent = () => {
+//   const customizedEls = [
+//     document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_JS)[0],
+//     document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_HTML)[0],
+//     document.getElementsByClassName(KINTONE_PORTAL_CUSTOMIZE_CSS)[0]
+//   ];
+//   customizedEls.filter(el => !!el).forEach(el => el.remove());
+// };
 
-const execUserScriptSample = () => {
+const execUserScriptSample = async () => {
+  chrome.userScripts.configureWorld({
+    csp: "script-src 'self'"
+  });
   chrome.userScripts.register([
     {
       id: "test",
